6eb7a4b479eb706cc8621655bfb2da835475881f
BraenAndDany <braenanddany@mail.com>
2023-10-24 12:03:58 +0700

    Добавьте файлы проекта.

---
+++ Task№6/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Task_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnApp = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.tbRenameBuck = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Список функций";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnApp
+            // 
+            this.btnApp.Location = new System.Drawing.Point(728, 483);
+            this.btnApp.Name = "btnApp";
+            this.btnApp.Size = new System.Drawing.Size(118, 38);
+            this.btnApp.TabIndex = 1;
+            this.btnApp.Text = "Применить";
+            this.btnApp.UseVisualStyleBackColor = true;
+            this.btnApp.Click += new System.EventHandler(this.btnApp_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 49);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(174, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Переименовывание корзины";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // tbRenameBuck
+            // 
+            this.tbRenameBuck.Location = new System.Drawing.Point(13, 73);
+            this.tbRenameBuck.Name = "tbRenameBuck";
+            this.tbRenameBuck.Size = new System.Drawing.Size(173, 20);
+            this.tbRenameBuck.TabIndex = 3;
+            this.tbRenameBuck.TextChanged += new System.EventHandler(this.tbRenameBuck_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(279, 257);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(187, 96);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 545);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.tbRenameBuck);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.btnApp);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "r";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnApp;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox tbRenameBuck;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Task№6/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using Microsoft.Win32;
+
+namespace Task_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void RenameBucket()
+        {
+            RegistryKey keyAuto = Registry.Users;
+            keyAuto = keyAuto.OpenSubKey("S-1-5-21-4214029148-1305989752-1630343126-1001\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\CLSID\\{645FF040-5081-101B-9F08-00AA002F954E}");
+            richTextBox1.Text = keyAuto.GetSubKeyNames().ToString();
+            //richTextBox1.Text = keyAuto.GetValue("Default");
+        }
+
+        private void btnApp_Click(object sender, EventArgs e)
+        {
+            RenameBucket();
+        }
+
+        private void tbRenameBuck_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+   
+}
